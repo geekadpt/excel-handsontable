3cf6857f287396839f525b7289ed8850067b27ae
mozilan <mozilan@aliyun.com>
2020-02-01 17:59:31 +0800

    发送短信验证码

--- routes/api.php
+++ routes/api.php
@@ -2,11 +2,18 @@
 
 use Illuminate\Http\Request;
 
+//路由前缀
 Route::prefix('v1')
+    //命名空间
     ->namespace('Api')
+    //别名
     ->name('api.v1.')
+    //路由组
     ->group(function () {
-        Route::get('version', function() {
-            return 'this is version v1';
-        })->name('version');
+        // 短信验证码
+        Route::post('verificationCodes', 'VerificationCodesController@store')
+            ->name('verificationCodes.store');
+        // 用户注册
+        Route::post('users', 'UsersController@store')
+            ->name('users.store');
     });
